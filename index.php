6751ee8920e685585af7da6d2e8255ad008b1d8a
Mr-Great <onosugreat@gmail.com>
2016-12-04 13:48:51 +0100

    Initiate Project User Interface Design

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Renny's Hotel n Suites</title>
+	<link rel="stylesheet" href="css/magic.css">
+</head>
+<body>
+	<div id="wrap-top">
+		<div class="navigation">
+			<div class="nav-text">
+				<h3>Renny's Hotel n Suites</h3>
+			</div>
+			<div class="nav-items"></div>
+		</div>
+		<div class="notice-board">
+			<div class="notice-text">
+				<h2>Find and book hotels in Nigeria.</h2>
+				<h4>Search through 8342 hotels in Nigeria</h4>
+			</div>
+			<div class="notice-book">
+				
+			</div>
+		</div>
+	</div>
+</body>
+</html>
